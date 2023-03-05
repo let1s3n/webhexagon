60e85e283046bc1abc03cc2fe3c5db1c8ca29b2d
let1s3n <letisendev@gmail.com>
2023-03-05 13:14:00 -0500

    fix

--- src/components/templates/Home/home.tsx
+++ src/components/templates/Home/home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Link from "next/link";
 import Image from "next/image";
 
 import { Container, Button } from "react-bootstrap";
@@ -52,9 +53,11 @@ const Home = () => {
             <div
               className={styles.customButtonContainer + " position-relative"}
             >
-              <Button variant="link" className={styles.customButton}>
-                CONTACTAR AHORA
-              </Button>
+              <Link href="/contacto" passHref>
+                <Button variant="link" className={styles.customButton}>
+                  CONTACTAR AHORA
+                </Button>
+              </Link>
               <div className={styles.buttonDecoration}></div>
             </div>
           </div>
@@ -63,14 +66,65 @@ const Home = () => {
         <div
           className={
             styles.infoBox +
-            " position-absolute top-100 start-50 translate-middle"
+            " position-absolute top-100 start-50 translate-middle text-white"
           }
-        ></div>
+        >
+          <h4 className="fs-6 fs-xl-4 mb-3">
+            ¡NOSOTROS PODEMOS <span className="text-terciary">AYUDARTE</span> A
+            LOGRARLO!
+          </h4>
+
+          <p
+            className="d-none d-xl-block fs-14 mb-4"
+            style={{ maxWidth: "362px" }}
+          >
+            Nos apasiona ayudar a las marcas con propósito a ganar en Internet y
+            aumentar su impacto positivo.
+          </p>
+
+          <Button variant="outline-white" className={styles.customButton}>
+            VER SERVICIOS
+          </Button>
+
+          <Image
+            src={`${process.env.NEXT_PUBLIC_CDN}images/home/cubosHome.png`}
+            alt="Cubos Home"
+            width={277}
+            height={0}
+            className="position-absolute d-none d-xl-block"
+            style={{ top: "-33px", right: "109px", height: "auto" }}
+          />
+        </div>
       </section>
 
       <section className={styles.proyects}>
         <Container className="g-0 mx-auto" style={{ width: "fit-content" }}>
-          <h1 className={styles.title}>PROYECTOS DESTACADOS</h1>
+          <div
+            className="d-flex flex-column align-items-center align-items-xl-stretch flex-xl-row justify-content-between"
+            style={{ rowGap: "1rem" }}
+          >
+            <div className="position-relative" style={{ width: "fit-content" }}>
+              <h1 className={styles.title}>PROYECTOS DESTACADOS</h1>
+              <Image
+                src={`${process.env.NEXT_PUBLIC_CDN}images/general/titleDecore.svg`}
+                alt="Title Decore"
+                width={216}
+                height={0}
+                className={styles.titleDecore}
+              />
+            </div>
+
+            <Link href="/proyectos" passHref>
+              <Button
+                variant="black"
+                className={
+                  styles.customButton + " border-white border-2 rounded-0"
+                }
+              >
+                <p className={styles.innerText}>VER PROYECTOS</p>
+              </Button>
+            </Link>
+          </div>
 
           <div
             className="d-flex flex-column mt-8 align-items-center align-items-xxl-stretch"
@@ -85,6 +139,43 @@ const Home = () => {
                 className={styles.imgProyect}
                 style={{ height: "auto" }}
               />
+
+              <div className={styles.right}>
+                <div className="d-flex" style={{ columnGap: "12px" }}>
+                  <div className={styles.tag}>
+                    <p className="lh-1">WEB</p>
+                  </div>
+                  <div className={styles.tag}>
+                    <p className="lh-1">IOS</p>
+                  </div>
+                  <div className={styles.tag}>
+                    <p className="lh-1">Android</p>
+                  </div>
+                </div>
+
+                <h3 className="pt-2">DUNDER MIFFLIN</h3>
+
+                <p className={styles.proyectDescription}>
+                  Nos apasiona ayudar a las marcas con propósito a ganar en
+                  Internet y aumentar su impacto positivo.
+                </p>
+
+                <div
+                  className="d-flex align-items-center"
+                  style={{ columnGap: "1rem", paddingTop: "27px" }}
+                >
+                  <p className="lh-1">Ir al proyecto</p>
+                  <Link href="/" passHref style={{ lineHeight: "12px" }}>
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_CDN}images/general/arrow.png`}
+                      alt="Arrow"
+                      width={39}
+                      height={0}
+                      style={{ height: "auto" }}
+                    />
+                  </Link>
+                </div>
+              </div>
             </div>
             <div className={styles.proyect}>
               <Image
@@ -95,6 +186,43 @@ const Home = () => {
                 className={styles.imgProyect}
                 style={{ height: "auto" }}
               />
+
+              <div className={styles.right}>
+                <div className="d-flex" style={{ columnGap: "12px" }}>
+                  <div className={styles.tag}>
+                    <p className="lh-1">WEB</p>
+                  </div>
+                  <div className={styles.tag}>
+                    <p className="lh-1">IOS</p>
+                  </div>
+                  <div className={styles.tag}>
+                    <p className="lh-1">Android</p>
+                  </div>
+                </div>
+
+                <h3 className="pt-2">DUNDER MIFFLIN</h3>
+
+                <p className={styles.proyectDescription}>
+                  Nos apasiona ayudar a las marcas con propósito a ganar en
+                  Internet y aumentar su impacto positivo.
+                </p>
+
+                <div
+                  className="d-flex align-items-center"
+                  style={{ columnGap: "1rem", paddingTop: "27px" }}
+                >
+                  <p className="lh-1">Ir al proyecto</p>
+                  <Link href="/" passHref style={{ lineHeight: "12px" }}>
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_CDN}images/general/arrow.png`}
+                      alt="Arrow"
+                      width={39}
+                      height={0}
+                      style={{ height: "auto" }}
+                    />
+                  </Link>
+                </div>
+              </div>
             </div>
             <div className={styles.proyect}>
               <Image
@@ -105,6 +233,43 @@ const Home = () => {
                 className={styles.imgProyect}
                 style={{ height: "auto" }}
               />
+
+              <div className={styles.right}>
+                <div className="d-flex" style={{ columnGap: "12px" }}>
+                  <div className={styles.tag}>
+                    <p className="lh-1">WEB</p>
+                  </div>
+                  <div className={styles.tag}>
+                    <p className="lh-1">IOS</p>
+                  </div>
+                  <div className={styles.tag}>
+                    <p className="lh-1">Android</p>
+                  </div>
+                </div>
+
+                <h3 className="pt-2">DUNDER MIFFLIN</h3>
+
+                <p className={styles.proyectDescription}>
+                  Nos apasiona ayudar a las marcas con propósito a ganar en
+                  Internet y aumentar su impacto positivo.
+                </p>
+
+                <div
+                  className="d-flex align-items-center"
+                  style={{ columnGap: "1rem", paddingTop: "27px" }}
+                >
+                  <p className="lh-1">Ir al proyecto</p>
+                  <Link href="/" passHref style={{ lineHeight: "12px" }}>
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_CDN}images/general/arrow.png`}
+                      alt="Arrow"
+                      width={39}
+                      height={0}
+                      style={{ height: "auto" }}
+                    />
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </Container>
