3235c9f4d14e822c747e81ebb0d3fcb328ac3298
let1s3n <letisendev@gmail.com>
2023-03-09 11:48:39 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['d2fap8ni6v2bu4.cloudfront.net'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'd2fap8ni6v2bu4.cloudfront.net',
-      },
-    ],
   },
   devIndicators: {
     buildActivity: false,
