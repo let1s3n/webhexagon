72b862553f68a041d16c695a9361bd410303337c
let1s3n <letisendev@gmail.com>
2023-03-11 11:58:20 -0500

    fix

--- src/components/templates/Home/home.tsx
+++ src/components/templates/Home/home.tsx
@@ -289,12 +289,19 @@ const Home = ({ imageProps }: any) => {
       </section>
 
       <Parallax
-        blur={10}
-        bgImage={`${process.env.NEXT_PUBLIC_CDN}images/home/bgHome4.jpg`}
-        bgImageAlt="background home 4"
-        strength={200}
+        /* blur={10} */
+        /* bgImage={`${process.env.NEXT_PUBLIC_CDN}images/home/bgHome4.jpg`} */
+        bgImage={`${process.env.NEXT_PUBLIC_CDN}images/home/bgHome5.png`}
+        bgImageAlt="background home 5"
+        strength={500}
         disabled={width <= 600 ? true : false}
       >
+        {/* <Background>
+          <img
+            src={`${process.env.NEXT_PUBLIC_CDN}images/home/bgHome5.png`}
+            alt="background home 5"
+          />
+        </Background> */}
         <section className={styles.contact}>
           <div id="contactMain" className={styles.main}>
             <Container className="g-0 text-white text-center">
