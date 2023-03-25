15ec05b49ee4bf412fcf8e785d1cd59b8458bb0b
let1s3n <letisendev@gmail.com>
2023-03-25 12:35:58 -0500

    fix

--- src/components/templates/Servicios/servicios.tsx
+++ src/components/templates/Servicios/servicios.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import styles from "./servicios.module.scss";
+import Image from 'next/image';
+import styles from './servicios.module.scss';
 const Servicios = () => {
   return (
     <section className={styles.introduction}>
       <h1>servicios</h1>
+      <Image
+        alt="teamwork image"
+        src="/images/teamwork.png"
+        width={595}
+        height={538}
+        quality={100}
+      />
     </section>
   );
 };
