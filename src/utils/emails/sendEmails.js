dd963a9085e7c049a071cc6271ecb5d55a66072a
let1s3n <letisendev@gmail.com>
2024-04-27 12:33:05 -0500

    fix

--- src/utils/emails/sendEmails.js
+++ src/utils/emails/sendEmails.js
@@ -5,8 +5,8 @@ import '../envConfig';
 
 AWS.config.update({
   region: 'us-east-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AMAZON_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY,
 });
 
 const ses = new AWS.SES({ apiVersion: 'latest' });
